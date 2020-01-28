677dd60454bd63fb009f7d644890d2f5b1d77942
KL4Kekc <KL4.Kekc95kg@gmail.com>
2020-01-28 16:57:09 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        final String CONST = "Привет,";
+        String myWord = " мир!";
+        System.out.println(CONST + myWord);
+    }
+}
